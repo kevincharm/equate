2d9d8f66f440340ec79d6f51a85f453c87aff74b
Kevin Tjiam <kevin@kevincharm.com>
2018-04-22 16:23:44 +0800

    untrack module.d.ts

--- src/module.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-/**
- * Compares two images. Returns `true` if the two images are sufficiently similar.
- * @param imageA A node Buffer object containing raw data of the first image to compare
- * @param imageB A node Buffer object containing raw data of the second image to compare
- * @param tolerancePercentage Percentage of pixels allowed to be different for two images to be considered the same
- */
-export declare const isMatch: (imageA: Buffer, imageB: Buffer, tolerancePercentage: number) => boolean;
